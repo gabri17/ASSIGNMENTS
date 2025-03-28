31a2c8fc377a55f10df92b553d1e6867799a3d99
Gabriele <gabrielevolani02@gmail.com>
2025-03-28 16:19:42 +0100

    variance computed properly

--- assignment-1/ex1.py
+++ assignment-1/ex1.py
@@ -15,6 +15,14 @@ f_3 = 0.75
 p_4 = 0.25
 f_4 = 1
 
+"""
+computed_mean = mean_1 * p_1 + mean_2 * p_2 + mean_3 * p_3 + mean_4 * p_4
+computed_variance_1 = (p_1*var_1 + p_2*var_2 + p_3*var_3 + p_4*var_4)
+computed_variance_2 = (p_1*((mean_1-computed_mean)**2) + p_2*((mean_2-computed_mean)) + p_3*((mean_3-computed_mean)) + p_4*((mean_4-computed_mean)))
+print("Computed variance 1: " + str(computed_variance_1))
+print("Computed variance 2: " + str(computed_variance_2))
+"""
+
 N = 1_000_000
 
 samples = []
@@ -40,7 +48,16 @@ print("Dataset mean: " + str(empirical_mean_dataset), "\nDataset variance: " + s
 computed_mean = mean_1 * p_1 + mean_2 * p_2 + mean_3 * p_3 + mean_4 * p_4
 print("Computed mean: " + str(computed_mean))
 
+computed_variance = (p_1*var_1 + p_2*var_2 + p_3*var_3 + p_4*var_4) + (p_1*((mean_1-computed_mean)**2) + p_2*((mean_2-computed_mean)**2) + p_3*((mean_3-computed_mean)**2) + p_4*((mean_4-computed_mean)**2))
+print("Computed variance: " + str(computed_variance))
+
+
 #Var(X) = E[Var(X|Y)] + Var(E[X|Y])
 #X is my conditional distribution
 #Y is the probability of each gaussian
 #X | Y is one of the gaussians
+
+#E[X] = E[X|Y1]P(Y1) + E[X|Y2]P(Y2) + E[X|Y3]P(Y3) + E[X|Y4]P(Y4)
+#E[X|Y] is given
+#E[Var(X|Y)] = p_1*var_1 + p_2*var_2 + p_3*var_3 + p_4*var_4
+#Var(E[X|Y]) = p_1*((mean_1-computed_mean)**2) + p_2*((mean_2-computed_mean)**2) + p_3*((mean_3-computed_mean)**2) + p_4*((mean_4-computed_mean)**4)
