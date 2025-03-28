be345aaa5a827751f6493d9bd39b2ea288b2b9ea
Gabriele <gabrielevolani02@gmail.com>
2025-03-28 15:47:31 +0100

    trying some stuff

--- assignment-1/ex1.py
+++ assignment-1/ex1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import random
 
 mean_1, var_1 = -2, 2
 mean_2, var_2 = 4, 1
@@ -6,19 +7,40 @@ mean_3, var_3 = 10, 3
 mean_4, var_4 = 15, 2
 
 p_1 = 0.15
+f_1 = 0.15
 p_2 = 0.25
+f_2 = 0.40
 p_3 = 0.35
-p_4 = 0.2
+f_3 = 0.75
+p_4 = 0.25
+f_4 = 1
 
 N = 1_000_000
 
-s_1 = np.random.normal(mean_1, np.sqrt(var_1), N)
-s_2 = np.random.normal(mean_2, np.sqrt(var_2), N)
-s_3 = np.random.normal(mean_3, np.sqrt(var_3), N)
-s_4 = np.random.normal(mean_4, np.sqrt(var_4), N)
+samples = []
 
-mean_dataset_1, var_dataset_1 = np.mean(s_1), np.var(s_1)
+for i in range(N):
+    U = random.random()
 
-#print(s.shape)
+    if(U <= f_1):
+        n = np.random.normal(mean_1, np.sqrt(var_1), 1)
+    elif (U <= f_2):
+        n = np.random.normal(mean_2, np.sqrt(var_2), 1)
+    elif (U <= f_3):
+        n = np.random.normal(mean_3, np.sqrt(var_3), 1)
+    else:
+        n = np.random.normal(mean_4, np.sqrt(var_4), 1)
+
+    samples.append(float(n[0]))
+
+#print(samples)
+empirical_mean_dataset, empirical_var_dataset = np.mean(samples), np.var(samples)
+print("Dataset mean: " + str(empirical_mean_dataset), "\nDataset variance: " + str(empirical_var_dataset))
+
+computed_mean = mean_1 * p_1 + mean_2 * p_2 + mean_3 * p_3 + mean_4 * p_4
+print("Computed mean: " + str(computed_mean))
 
 #Var(X) = E[Var(X|Y)] + Var(E[X|Y])
+#X is my conditional distribution
+#Y is the probability of each gaussian
+#X | Y is one of the gaussians
