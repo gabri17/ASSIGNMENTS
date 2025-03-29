68b59be88a27a90a9657215bfe4fd7dbbbf550de
Gabriele <gabrielevolani02@gmail.com>
2025-03-29 13:58:33 +0100

    better + some graphs we love

--- assignment-1/ex1.py
+++ assignment-1/ex1.py
@@ -1,6 +1,9 @@
 import numpy as np
 import random
 
+import matplotlib.pyplot as plt
+
+"""
 mean_1, var_1 = -2, 2
 mean_2, var_2 = 4, 1
 mean_3, var_3 = 10, 3
@@ -14,42 +17,102 @@ p_3 = 0.35
 f_3 = 0.75
 p_4 = 0.25
 f_4 = 1
+"""
 
-"""
-computed_mean = mean_1 * p_1 + mean_2 * p_2 + mean_3 * p_3 + mean_4 * p_4
-computed_variance_1 = (p_1*var_1 + p_2*var_2 + p_3*var_3 + p_4*var_4)
-computed_variance_2 = (p_1*((mean_1-computed_mean)**2) + p_2*((mean_2-computed_mean)) + p_3*((mean_3-computed_mean)) + p_4*((mean_4-computed_mean)))
-print("Computed variance 1: " + str(computed_variance_1))
-print("Computed variance 2: " + str(computed_variance_2))
-"""
+class ConditionalGaussian:
+
+    def __init__(self, means, variances, probabilities):
+        if(sum(probabilities) != 1):
+            print("Probabilities do not respect probabilities axioms")
+
+        if(len(probabilities) != len(means) & len(means) != len(variances)):
+            print("Not ok data")
+        
+        self.means = means
+        self.variances = variances
+        self.probabilities = probabilities
+        f = []
+        for i in range(len(self.probabilities)):
+            if i != 0:
+                f.append(f[i-1] + self.probabilities[i])
+            else:
+                f.append(self.probabilities[i])
+        
+        self.f = f
+
+    
+    def get_conditioanl_mean(self):
+        return np.dot(self.means, self.probabilities)
+    
+    def get_conditional_variance(self):
+        expected_variance = np.dot(self.variances, self.probabilities)
+        computed_mean = self.get_conditioanl_mean()
+        variance_of_expectation = np.dot(list(map(lambda x: (x-computed_mean)**2, self.means)), self.probabilities)
+        return expected_variance + variance_of_expectation
+    
+    def sample(self):
+        U = random.random()
+
+        for i in range(len(self.f)):
+            if i == 0:
+                if(U <= self.f[i]):
+                    return float(np.random.normal(self.means[i], np.sqrt(self.variances[i]), 1)[0])
+            else:
+                if((U > self.f[i-1]) & (U <= self.f[i])):
+                    return float(np.random.normal(self.means[i], np.sqrt(self.variances[i]), 1)[0])
+                else:
+                    continue
+
+myvar = ConditionalGaussian([-2, 4, 10, 15], [2, 1, 3, 2], [0.15, 0.25, 0.35, 0.25])
+print("Theoretical mean: " + str(myvar.get_conditioanl_mean()))
+print("Theoretical variance: " + str(myvar.get_conditional_variance()))
 
 N = 1_000_000
 
 samples = []
+means, variances = [], []
+examples = []
+
+offset = 10_000
 
 for i in range(N):
-    U = random.random()
+    samples.append(myvar.sample())
+    
+    if(i < offset):
+        examples.append(i)
+        means.append(float(np.mean(samples)))
+        variances.append(np.var(samples))
 
-    if(U <= f_1):
-        n = np.random.normal(mean_1, np.sqrt(var_1), 1)
-    elif (U <= f_2):
-        n = np.random.normal(mean_2, np.sqrt(var_2), 1)
-    elif (U <= f_3):
-        n = np.random.normal(mean_3, np.sqrt(var_3), 1)
-    else:
-        n = np.random.normal(mean_4, np.sqrt(var_4), 1)
+yticks1 = 1
+yticks2 = 1
 
-    samples.append(float(n[0]))
+plt.title('Means against number of examples')
+plt.xlim(0, offset)
+plt.ylim(0, myvar.get_conditioanl_mean()*2)
+plt.yticks(np.arange(0, myvar.get_conditioanl_mean()*2, yticks1))
+plt.plot(examples, means)
+plt.savefig('./assignment-1/mean.png')
+
+
+plt.title('Variances against number of examples')
+plt.xlim(0, offset)
+plt.ylim(0, myvar.get_conditional_variance()*(3/2))  
+#plt.yticks(np.arange(0, myvar.get_conditional_variance()*2, yticks2))
+plt.plot(examples, variances)
+plt.savefig('./assignment-1/variance.png')
+
 
 #print(samples)
 empirical_mean_dataset, empirical_var_dataset = np.mean(samples), np.var(samples)
 print("Dataset mean: " + str(empirical_mean_dataset), "\nDataset variance: " + str(empirical_var_dataset))
 
+"""
 computed_mean = mean_1 * p_1 + mean_2 * p_2 + mean_3 * p_3 + mean_4 * p_4
 print("Computed mean: " + str(computed_mean))
 
 computed_variance = (p_1*var_1 + p_2*var_2 + p_3*var_3 + p_4*var_4) + (p_1*((mean_1-computed_mean)**2) + p_2*((mean_2-computed_mean)**2) + p_3*((mean_3-computed_mean)**2) + p_4*((mean_4-computed_mean)**2))
 print("Computed variance: " + str(computed_variance))
+"""
 
 
 #Var(X) = E[Var(X|Y)] + Var(E[X|Y])
